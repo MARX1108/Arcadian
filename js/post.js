c674c9dbc383353de1e68876fc5bb976932e6aed
Marx.W <boyuan@vt.edu>
2019-10-08 14:55:54 -0400

    updated

--- js/post.js
+++ js/post.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     $("#btn-post").click(function() {
         //       $(''<div> you successfully post a comment </div>").insertAfter( "#post_win" );
-        $( "<div class = 'post' id = 'success'>You successfully post a comment :)</div>"
+
+
+        var post = $('#post_box').val();
+
+        $( "<div class = 'post' id = ''> <p><a href='#'>Me</a></p><p>" + post + " </p><p id='time_stamp'>Just now</p></div>"
         ).insertBefore("#post_win");
 
+            $( "<div class = 'post' id = 'success'>You successfully post a comment :)</div>"
+    ).insertBefore("#post_win");
         setTimeout(function() {
             $("#success").remove();
         }, 4000);
