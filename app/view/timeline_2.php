c5af384c80622fdaebe19af6e7ef5bc336c55dde
Marx.W <boyuan@vt.edu>
2019-11-10 14:08:35 -0500

    timeline_send

--- app/view/timeline_2.php
+++ app/view/timeline_2.php
@@ -1,42 +1,44 @@
 <?php if(isset($_SESSION['username'])): ?>
 
-<div class="main row">
-
-    <div class="card col-md-2 mr-5 p-1 mt-4">
+<div class="main">
+    <ul id="secondary-nav">
         <li class="selected">
             <a href="<?= BASE_URL ?>/profile/following">Following</a>
         </li>
+        <!-- <li><a href="./profile_timeline.html">Timeline</a></li> -->
         <li>
             <a href="#" id="current_tab">Timeline</a>
         </li>
-    </div>
+    </ul>
 
-    <div class="card col-md-6 p-1  m-2 mr-5" id="middle_container">
-
+    <div id="content-left">
         <form id="post" method="POST" action="<?= BASE_URL ?>/profile/submit">
-            <div class="post card my-4 p-4" id="post_win">
-                <label for="title" class="h5 ml-2 ">Title</label>
-                <input id="title" name="title" type="text" placeholder="Titles goes here" class="form-control">
+            <div class="post" id="post_win">
+                <label for="title">Title</label>
+                <input id="title" name="title" type="text" placeholder="Titles goes here">
 
-                <label for="url" class="h5  ml-2 mt-2">URL (required)</label>
-                <input id="url" name="url" type="text" placeholder="URL goes here" class="form-control">
+                <label for="url">URL (required)</label>
+                <input id="url" name="url" type="text" placeholder="URL goes here">
 
-                <label for="tags" class="h5  ml-2 mt-2">Tags </label>
-                <input id="url" name="tags" type="text" placeholder="Tags goes here" class="form-control">
+                <label for="tags">Tags </label>
+                <input id="url" name="tags" type="text" placeholder="Tags goes here">
 
-                <label for="img_url" class="h5 ml-2 mt-2">URL of Your Art Work (required)</label>
-                <input id="img_url" name="img_url" type="text" placeholder="URL of Your Art Work" class="form-control">
+                <label for="img_url">URL of Your Art Work (required)</label>
+                <input id="img_url" name="img_url" type="text" placeholder="URL of Your Art Work">
 
-                <textarea id="description" name="description" placeholder="Description goes here. "
-                    class="form-control my-4 mt-2"></textarea>
-                <button id="upload" type="submit" class="btn btn-primary">Post</button>
+                <textarea id="description" name="description" placeholder="Description goes here. "></textarea>
+                <button id="upload" type="submit">Post</button>
             </div>
         </form>
 
+        <!-- <div class="post" id="post_win">
+            <textarea placeholder="Post Something"></textarea>
+            <button id="post">Post</button>
+            <button id="upload">Upload</button>
+        </div> -->
         <?php foreach($stories as $story): ?>
         <?php if(strcmp(($story->author), $_SESSION['username'])== 0):?>
-        <div class="post card m-2 p-4">
-
+        <div class="post">
             <p><a href="#"><?=$story->title?></a> by <?=$story->author?></p>
             <ul class="post_pic">
                 <a href="<?= BASE_URL ?>/detail/<?= $story->id ?>"><img src="<?= $story->img_url ?>" width="80%"
@@ -56,7 +58,7 @@
 
 
     </div>
-    <div class="card col-md-3 mt-4 ml-auto p-3">
+    <div id="content-right">
         <h2>Welcome to Arcadian</h2>
         <p>
             Arcadian is a user-generate-content online community which targets college students who are interested
@@ -69,11 +71,11 @@
     <?php else: ?>
     <div class="main container text-center">
         <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div> -->
+    <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div> --> 
         <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div> -->
-        <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div> -->
-        <img src="<?= BASE_URL ?>/public/assets/padlock.png" alt="" width="10%" href="<?= BASE_URL ?>/home">
-        </br>
-
-        <a href="<?= BASE_URL ?>/home" class="mt-4">Click Here To Sign in</a>
+        <img src="<?= BASE_URL ?>/public/assets/padlock.png" alt="" width = "10%"  href="<?= BASE_URL ?>/home" > 
+    </br>
+    
+        <a href="<?= BASE_URL ?>/home" class = "mt-4">Click Here To Sign in</a>
         <?php endif; ?>
     </div>
