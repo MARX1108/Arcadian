9e23dc49d6b3d7c48ea0aa3e38805a1e0419dcbd
Marx.W <boyuan@vt.edu>
2019-11-10 13:00:41 -0500

    update with bootstrap

--- app/view/header.php
+++ app/view/header.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- fix Chrome device emulator -->
     <title>Arcadian</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/<?=$stylesheet?>" />
 
     <script src="<?= BASE_URL ?>/public/js/jquery.js"></script>
     <script src="<?= BASE_URL ?>/public/js/topnews.js"></script>
+
+    <!-- Credit to Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 </head>
+
 <body>
     <!-- header-->
     <div id="header">
-        <h1>Arc<a id = "logo" >a</a>dian</h1>
+        <h1>Arc<a id="logo">a</a>dian</h1>
         <ul id="primary-nav">
             <li>
-                <a href="<?= BASE_URL ?>/home" id = "<?=$all_state?>"  class = "current_nav">All</a>
+                <a href="<?= BASE_URL ?>/home" id="<?=$all_state?>" class="current_nav">All</a>
             </li>
             <li>
-                <a href="<?= BASE_URL ?>/discover" id = "<?=$discover_state?>">Discover</a></li>
+                <a href="<?= BASE_URL ?>/discover" id="<?=$discover_state?>">Discover</a></li>
             <li>
-                <a href="<?= BASE_URL ?>/profile/timeline" id = "<?=$profile_state?>">Profile</a></li>
+                <a href="<?= BASE_URL ?>/profile/timeline" id="<?=$profile_state?>">Profile</a></li>
         </ul>
         <form id="search-bar">
             <input type="text" placeholder=" Search Something" />
--- app/view/home.php
+++ app/view/home.php
@@ -1,61 +1,75 @@
     <!-- body content section -->
-    <div id="news">
-        <div id="topnews">
-            <!-- <img src= "../assets/all_topnews.png">-->
+    <div id="news" class="row h-20">
+
+        <div class="card col-md-7 mr-0 ml-5 container">
+            <div class="card-body">
+                <!-- <div id="topnews"> -->
+                <img src="<?= BASE_URL ?>/public/assets/all_topnews.png" width="100%">
+                <!-- </div> -->
+            </div>
         </div>
 
-        <div id="signin">
-           
-            <!-- <div> -->
-            <form id="login" method="POST" action="<?= BASE_URL ?>/login/process">
+        <div class="card col-md-4 ml-4 container">
+            <div class="card-body container">
+                <div id="signin">
 
-                <?php if(isset($_SESSION['username'])): ?>
-                <p>Welcome, <strong><?= $_SESSION['username'] ?></strong> 
-                </br>
-                <a id="btn-signup" href="<?= BASE_URL ?>/logout" >Log Out</a></p>
-                <?php else: ?>
-                <p>Welcome Back!</p>
-                
+                    <!-- <div> -->
+                    <form id="login" method="POST" action="<?= BASE_URL ?>/login/process">
 
-                <?php if(isset($_SESSION['msg'])): ?>
-                <div class="success">
-                    <?= $_SESSION['msg'] ?>
+                        <?php if(isset($_SESSION['username'])): ?>
+                        <p>Welcome, <strong><?= $_SESSION['username'] ?></strong>
+                            </br>
+                            <a id="btn-signup" href="<?= BASE_URL ?>/logout">Log Out</a></p>
+                        <?php else: ?>
+                        <p>Welcome Back!</p>
+
+
+                        <?php if(isset($_SESSION['msg'])): ?>
+                        <div class="success">
+                            <?= $_SESSION['msg'] ?>
+                        </div>
+                        <?php unset($_SESSION['msg']); ?>
+                        <?php endif; ?>
+
+                        <input id="un" name="un" type="text" placeholder=" Username" />
+                        <input id="pw" name="pw" type="text" placeholder=" Password" />
+                        <button id="btn-signin">sign in</button>
+                        <button id="btn-signup" disabled>sign up</button>
+                        <?php endif; ?>
+
+
+                    </form>
+
+                    <!-- </div> -->
                 </div>
-                <?php unset($_SESSION['msg']); ?>
-                <?php endif; ?>
-
-                <input id="un" name="un" type="text" placeholder=" Username" />
-                <input id="pw" name="pw" type="text" placeholder=" Password" />
-                <button id="btn-signin">sign in</button>
-                <button id="btn-signup" disabled>sign up</button>
-                <?php endif; ?>
-
-
-            </form>
-
-            <!-- </div> -->
+            </div>
         </div>
 
     </div>
 
     <div class="main">
-    
+
         <?php 
         $count = 0;
         foreach($stories as $story): 
         $count = $count + 1;
         ?>
-        
+
         <?php if($count% 4 == 1): ?>
-        <ul class="content">
-        <?php endif; ?>
-            <li>
-                <a href="<?= BASE_URL ?>/detail/<?= $story->id ?>"><img src="<?= $story->img_url ?>" width="20%"
-                        alt="001" /></a>
+        <!-- <ul class="content"> -->
+        <div id="news" class="row card-deck">
+
+            <?php endif; ?>
+            <div class="card pic_story">
+                <!-- <li> -->
+                <a href="<?= BASE_URL ?>/detail/<?= $story->id ?>"><img src="<?= $story->img_url ?>" class="pic"
+                        width="100%" alt="001" /></a>
                 <!--source: <?= $story->url ?> -->
-            </li>
+                <!-- </li> -->
+            </div>
             <?php if($count % 4 == 0): ?>
-        </ul>
+            <!-- </ul> -->
+        </div>
         <?php endif; ?>
         <?php endforeach; ?>
         </ul>
