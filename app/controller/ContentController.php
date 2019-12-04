8742de47adf537fad198fb8dfca43f5b39ea9aec
Marx.W <boyuan@vt.edu>
2019-12-03 20:32:57 -0500

    d

--- app/controller/ContentController.php
+++ app/controller/ContentController.php
@@ -427,7 +427,7 @@ class ContentController {
     $ev->story_1_id = $story->id;
     $ev = Event::insertEvent($ev);
 
-    $url = "'.BASE_URL.'/detail/'.$story->id";
+    $url = BASE_URL."/detail/".$story->id;
     echo json_encode(array("content" => 'post success', "url" => $url, "id" => $creator_id, "description" => $description));
   }
   public function save_editing_process()
